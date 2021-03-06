2578379085af8fcf5c66d6d34c298a11c2900b16
1706040 <1706040@kiit.ac.in>
2021-03-07 01:33:45 +0530

    rollback to public password

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: 'pratick00714@gmail.com',
-      pass: process.env.PASS
+      pass: 'letsread'
     }
 });
 
